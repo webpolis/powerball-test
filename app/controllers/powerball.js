52e448f446a626da9d203830577fce49e3c0a2ce
Nicolas Iglesias <nico@webpolis.com.ar>
2016-02-12 18:50:59 -0300

    simplify code

--- app/controllers/powerball.js
+++ app/controllers/powerball.js
@@ -8,42 +8,31 @@ const pbConfig = require('../config/powerball');
 import importer from '../importers/powerball';
 
 const pb = new importer();
+const error = (err) => {
+    console.log(err);
+
+    res.send({
+        error: err
+    });
+};
 
 router.get('/jackpot', apicache(pbConfig.cache.jackpot), (req, res) => {
     pb.fetch().then((data) => res.send({
         jackpot: data[0]
-    })).catch((err) => {
-        console.log(err);
-
-        res.send({
-            error: err
-        });
-    });
+    })).catch(error);
 });
 
 router.get('/winners', apicache(pbConfig.cache.winners), (req, res) => {
     pb.fetch().then((data) => res.send({
         winners: data[1]
-    })).catch((err) => {
-        console.log(err);
-
-        res.send({
-            error: err
-        });
-    });
+    })).catch(error);
 });
 
 router.get('/all', apicache(pbConfig.cache.all), (req, res) => {
     pb.fetch().then((data) => res.send({
         jackpot: data[0],
         winners: data[1]
-    })).catch((err) => {
-        console.log(err);
-
-        res.send({
-            error: err
-        });
-    });
+    })).catch(error);
 });
 
 module.exports = router;
