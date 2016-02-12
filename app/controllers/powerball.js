dfc83b90ae997b1287beef3ec5038577d743b058
Nicolas Iglesias <nico@webpolis.com.ar>
2016-02-12 18:31:36 -0300

    prepare routes

--- app/controllers/powerball.js
+++ app/controllers/powerball.js
@@ -1,16 +1,27 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
+const apicache = require('apicache').options({
+    debug: true
+}).middleware;
+const pbConfig = require('../config/powerball');
 
 import importer from '../importers/powerball';
 
 const pb = new importer();
 
-router.get('/jackpot', (req, res) => {
-    pb.init().then((data) => res.json(data.jackpot));
+router.get('/jackpot', apicache(pbConfig.cache.jackpot), (req, res) => {
+    pb.fetch().then((data) => res.send(data.jackpot)).catch((err) => console.log(err));
 });
 
-router.get('/winners', (req, res) => {
-    pb.init().then((data) => res.json(data.winners));
+router.get('/winners', apicache(pbConfig.cache.winners), (req, res) => {
+    pb.fetch().then((data) => res.send(data.winners)).catch((err) => console.log(err));
+});
+
+router.get('/all', apicache(pbConfig.cache.all), (req, res) => {
+    pb.fetch().then((data) => res.send({
+        jackpot: data[0],
+        winners: data[1]
+    })).catch((err) => console.log(err));
 });
 
 module.exports = router;
