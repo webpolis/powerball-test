eafb7829b5120f3d6397488070aea00b5616ee47
Nicolas Iglesias <nico@webpolis.com.ar>
2016-02-12 18:38:54 -0300

    return object for indivual paths

--- app/controllers/powerball.js
+++ app/controllers/powerball.js
@@ -10,11 +10,15 @@ import importer from '../importers/powerball';
 const pb = new importer();
 
 router.get('/jackpot', apicache(pbConfig.cache.jackpot), (req, res) => {
-    pb.fetch().then((data) => res.send(data.jackpot)).catch((err) => console.log(err));
+    pb.fetch().then((data) => res.send({
+        jackpot: data[0]
+    })).catch((err) => console.log(err));
 });
 
 router.get('/winners', apicache(pbConfig.cache.winners), (req, res) => {
-    pb.fetch().then((data) => res.send(data.winners)).catch((err) => console.log(err));
+    pb.fetch().then((data) => res.send({
+        winners: data[1]
+    })).catch((err) => console.log(err));
 });
 
 router.get('/all', apicache(pbConfig.cache.all), (req, res) => {
